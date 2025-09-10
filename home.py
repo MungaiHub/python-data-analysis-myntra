cc8991f898185aac5f6a95f575202e2418e01c35
mungaiHub <amosmungai085@gmail.com>
2025-09-10 18:51:01 +0300

    displayed sales data in Streamlit dashboard

--- home.py
+++ home.py
@@ -1,8 +1,18 @@
 import streamlit as st
-import pandas as pandas
+import pandas as pd
 import plotly.express as px
 from streamlit_option_menu import option_menu
 from numerize.numerize import numerize
+from query  import *
 
 st.set_page_config(page_title="Dashboard",layout="wide")
 st.subheader("Myntra Sales Descriptive Analytics")
+st.markdown('##')
+
+
+
+
+result=view_all_data()
+columns = ["id","brand_name", "pants_description", "price", "MRP", "discount_percent", "ratings", "number_of_ratings"]
+df = pd.DataFrame(result, columns=columns)
+st.dataframe(df)
---
+++ query.py
@@ -0,0 +1,19 @@
+import mysql.connector
+import streamlit as st
+
+#connection
+
+conn=mysql.connector.connect(
+    host="localhost",
+    port="3306",
+    user="root",
+    password="",
+    database="mydatabase"
+)
+c=conn.cursor()
+
+#fetch
+def view_all_data():
+    c.execute('select* from myntra order by id asc ')
+    data=c.fetchall()
+    return data

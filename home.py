3ddf8e207a24032b90759d9e5b959bbdb95ea5ff
mungaiHub <amosmungai085@gmail.com>
2025-09-11 18:50:48 +0300

    enhanced Streamlit dashboard with analytics

--- home.py
+++ home.py
@@ -11,8 +11,69 @@ st.markdown('##')
 
 
 
-
+#fetch data
 result=view_all_data()
 columns = ["id","brand_name", "pants_description", "price", "MRP", "discount_percent", "ratings", "number_of_ratings"]
 df = pd.DataFrame(result, columns=columns)
-st.dataframe(df)
+
+#sidebar
+st.sidebar.image("data/logo.jpg", caption="sales analytics")
+
+#switcher
+st.sidebar.header("please filter")
+brand_name=st.sidebar.multiselect(
+    "select brand name",
+    options=df["brand_name"].unique(),
+    default=df["brand_name"].unique(),
+)
+price=st.sidebar.multiselect(
+    "select price",
+    options=df["price"].unique(),
+    default=df["price"].unique(),
+)
+number_of_ratings=st.sidebar.multiselect(
+    "select number of ratings",
+    options=df["number_of_ratings"].unique(),
+    default=df["number_of_ratings"].unique(),
+)
+
+df_selection=df.query(
+    "brand_name==@brand_name & price==@price & number_of_ratings==@number_of_ratings"
+)
+
+def Home():
+    with st.expander("Tabular"):
+        showData=st.multiselect('Filter: ',df_selection.columns, default=[])
+        st.write(df_selection[showData])
+
+#compute top analytics
+total_price=df_selection["price"].sum()
+mode_price=df_selection["price"].mode()
+mean_price=df_selection["price"].mean()
+median_price=df_selection["price"].median()
+rating=df_selection["ratings"].sum()
+
+total1,total2,total3,total4,total5=st.columns(5, gap='large')
+with total1:
+    st.info('Total price')
+    st.metric(label="sum Kshs", value=f"{total_price:,.0f}")
+
+with total2:
+    st.info('most frequent')
+    mode_price = df_selection["price"].mode()[0] 
+    st.metric(label="mode Kshs", value=f"{mode_price:,.0f}")
+
+with total3:
+    st.info('Averange')
+    st.metric(label="averange Kshs", value=f"{mean_price:,.0f}")
+
+with total4:
+    st.info('Central Earnings')
+    st.metric(label="median Kshs", value=f"{median_price:,.0f}")
+
+with total5:
+    st.info('Ratings')
+    st.metric(label="Rating", value=numerize(rating), help=f"""Total Rating: {rating}""")
+
+st.markdown("""---""")
+Home()

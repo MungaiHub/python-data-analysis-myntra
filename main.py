2db9f8abb242be48e27f760ae7f3fd33cb5b7509
mungaiHub <amosmungai085@gmail.com>
2025-10-01 12:00:03 +0300

    add function to print dataframe and compute top analytics

--- main.py
+++ main.py
@@ -31,5 +31,42 @@ Business_type=st.sidebar.multiselect("Select the Business Type:",options=df["Bus
 df_selection=df.query(
     "Region == @region & Location==@location & BusinessType == @Business_type"
 )
-  
 
+def HomePage():
+    #print dataframe""
+    with st.expander("🛢 My database"):
+        showdata=st.multiselect("Filter:",df_selection.columns,default=["BusinessName","Location","Region","BusinessType","Investment","ConstructionYear","Rating"])
+        st.dataframe(df_selection[showdata],use_container_width=True)
+
+    #compute top analytics
+    total_investment=float(df_selection['Investment'].sum())
+    investment_mode=float(df_selection['Investment'].mode())
+    investment_mean=float(df_selection['Investment'].mean())
+    investment_median=float(df_selection['Investment'].median())
+    rating=float(df_selection['Rating'].sum())
+
+    #columns
+    total1,total2,total3,total4,total5=st.columns(5,gap='large')
+    with total1:
+        st.info('Total Investment',icon="📌")
+        st.metric(label='sum Kshs',value=f"{total_investment:,.0f}")
+
+    with total2:
+        st.info('Most Frequently',icon="📌")
+        st.metric(label='mode Kshs',value=f"{investment_mode:,.0f}")
+
+    with total3:
+        st.info('Investment Averange',icon="📌")
+        st.metric(label='mean Kshs',value=f"{investment_mean:,.0f}")
+
+    with total4:
+        st.info('Investment Marging',icon="📌")
+        st.metric(label='median Kshs',value=f"{investment_median:,.0f}")
+
+    with total5:
+        st.info('Ratings',icon="📌")
+        st.metric(label="Rating",value=numerize(rating),help=f"""Total rating: {rating}""")
+
+    st.markdown("""---""")
+
+HomePage()
